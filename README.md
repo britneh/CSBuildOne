0cf970d7d4190697454dec42ee6d4b2ca1fa3cda
britneh <brhacket@gmail.com>
2020-09-23 11:54:43 -0700

    something working and a bit of writeup in readme

---
+++ README.md
@@ -0,0 +1,22 @@
+1. Pick k random points as cluster centers
+2. assign each xi to nearest cluster by calculating distance to each centroid (euclidean distance)
+3. Find new cluster center by taking the average of assigned points
+4. Repeat step 2-3 until none of the cluster assignments change 
+
+Clustering is dividing a dataset into groups, consisting of similiar data points 
+    - Group of diners 
+        People at different tables not related to each other
+    - Items arranged in a grocery store
+        Similiar items are grouped together; produce
+
+Netflix, Amazon - use to make recommendations, based on previous purchases are previously watched 
+
+Kmeans is a type of exclusive clustering.  There also exists overlapping and heirarchical clustering. 
+
+
+Step 1: 
+Select the number of clusters to be identified
+
+Step 2: Randomly select 3 distinct data points
+
+Step 3: Measure distance between the first point and the 3 clusters 
--- kmeans.py
+++ kmeans.py
@@ -1,3 +1,6 @@
+import numpy as np
+import scipy.spatial.distance as scidist
+
 class Kmeans():
     def __init__(self, clusters):
         self.centers = clusters
@@ -12,8 +15,9 @@ class Kmeans():
             #randomly choose k examples as initial centroids
             random = np.random.randint(len(self.X))
             if i != 0:
-                if rand != self.centroid_i[i-1]:
-                    #assign each example to closest centroid
+                if random != self.centroid_i[i-1]:
+                    #assign each example to closest centroid; the more similiar 
+                    #the closer to the starting point 
                     self.centroid_i.append(random)
                     i += 1
             else:
@@ -26,8 +30,43 @@ class Kmeans():
             self.centroids.append(centroid)
     
     def get_centroids(self, X, nearest_cent):
-    
-    def find_nearest(self, df, centroids):
+        sums = []
+        counts = []
+        averages = []
+        for i in range(len(X[0])):
+            sum = [0] * self.centers
+            count = [0] * self.centers
+            
+            for j in range(len(self.X)):
+                sum[nearest_cent[j]] += float(self.X[j][i])
+                count[nearest_cent[j]] += 1
+            sums.append(sum)
+            counts.append(count)
+        for i in range(len(counts)):
+            average = []
+            for j in range(len(counts[0])):
+                average.append((sums[i][j])/(counts[i][j]))
+            averages.append(average)
+
+        averages = list(map(list, zip(*averages)))
+        return averages 
+
+    def find_nearest(self, df, cntrds):
+        last_cent = [np.random.choice([0,1,2])]*len(X)
+        i = 0
+        cntrds = self.centroids.copy()
+        while True:
+            if i>0:
+                cntrds = self.get_centroids(X, last_cent)
+            distances = scidist.cdist(X, cntrds)
+            nearest_cent = np.argmin(distances, axis =1)
+
+            if (list(nearest_cent) == list(last_cent)):
+                centroids = cntrds
+                return nearest_cent
+            else:
+                i += 1
+                last_cent = nearest_cent 
 
     def fit(self, X):
         self.X = X
@@ -46,4 +85,36 @@ class Kmeans():
 
     
     def final_cents(self):
+        for i in self.centroids:
+            print(i)
+
+if __name__ == '__main__':
+    # Read in data from csv to X
+    with open('CSOne.csv','r') as file:
+        X = []
+        for i, line in enumerate(file):
+            if i != 0:
+                words = line.split(',')
+                row = [words[1]]
+                row.append(words[2])
+                X.append(row)
+
+    # Display first 10 rows:
+    print('\n'*2, "*** First 10 rows of X ***")
+    print(X[:10])
+    print()
             
+  # Create new KMeans object
+    kmeans = Kmeans(3)
+
+    # Fit the model: assigns each point in X to a cluster
+    kmeans.fit(X)
+    print("*** Centroids ***")
+    print(kmeans.final_cents())
+    print()
+
+    print("*** Clusters ***", kmeans.labels)
+    print()
+    
+    sample = [[6,9],[6,11]]
+    print(f"*** Prediction for {sample} *** \n", kmeans.predict([[6,9],[6,11]]),'\n')
